2a9e99d7af27b99c99ac319c5350c2fdbf6ca70e
jbh2032 <jbh2032@naver.com>
2022-08-23 17:57:57 +0900

    static파일설정완료

--- app/app.js
+++ app/app.js
@@ -2,9 +2,11 @@
 const express = require("express");
 const app = express();
 const home = require("./src/routes/home");
+const path = require("path");
 
 app.set("views",__dirname+"/src/views/home");
 app.set("view engine","ejs");
 app.use("/",home); // 미들웨어 등록해주는 메소드
+app.use("/js",express.static("./src/public/js"));
 
 module.exports = app;;
