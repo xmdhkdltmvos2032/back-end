0eb8fa5d31b291a10da6b846a72ff808c4425fdd
jbh2032 <jbh2032@naver.com>
2022-08-23 16:31:26 +0900

    ewewe

--- src/bin/www.env.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-
-const Port = 3000;
-const app = require("../app");
-
-app.listen(Port, () => {
-    console.log("서버가동");
-})
--- src/routes/home/home.ctrl.js
+++
@@ -1,9 +0,0 @@
-const home = (req,res)=>{
-    res.render("index")
-}
-
-const login = (req, res) => {
-    res.render("login");
-}
-
-module.exports = {home,login}
