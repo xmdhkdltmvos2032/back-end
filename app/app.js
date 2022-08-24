bf344ddc574859d9bbc7ff52dd3cdf39fce64b7b
jbh2032 <jbh2032@naver.com>
2022-08-24 20:07:03 +0900

    초기화

--- app/app.js
+++ app/app.js
@@ -1,12 +1,15 @@
 "use strict"
 const express = require("express");
 const app = express();
-const home = require("./src/routes/home");
-const path = require("path");
+const router = require("./src/routes/home/router");
+const bodyParser = require("body-parser");
 
-app.set("views",__dirname+"/src/views/home");
+app.set("views",__dirname+"/src/views/home/");
 app.set("view engine","ejs");
-app.use("/",home); // 미들웨어 등록해주는 메소드
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use("/",router); // 미들웨어 등록해주는 메소드
 app.use("/js",express.static("./src/public/js"));
 
-module.exports = app;;
+
+module.exports = app;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,9 +1,33 @@
-const home = (req,res)=>{
-    res.render("index")
+"use strict"
+const UserStorage = require("../../models/UserStorage");
+
+const output = {
+    home: (req, res) => {
+        res.render("index")
+    },
+    login: (req, res) => {
+        res.render("login");
+    }
 }
 
-const login = (req, res) => {
-    res.render("login");
+const process = {
+    login: (req, res) => {
+        const id = req.body.id;
+        const pw = req.body.pw;
+
+        const response = {};
+        // if (users.loginData.id.includes(id)) {
+        //     const idx = users.loginData.id.indexOf(id);
+        //     if (users.loginData.pw[idx] === pw) {
+        //         response.성공 = true
+        //         return res.json(response);
+        //     }
+        // }
+        UserStorage.getUsers("id","aaa","bbb","ccc");
+        response.성공 = false;
+        response.msg = "로그인에 실패하셨습니다.";
+        return res.json(response)
+    }
 }
 
-module.exports = {home,login}
+module.exports = { output, process }
